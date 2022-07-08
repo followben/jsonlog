c66d0d82e07cea0ef814f6e1cbb54a05c47fd550
Alex Liang <alex.liang@saberastro.com>
2022-07-08 17:45:22 +1000

    stringify uuid and datetimes in extras

--- jsonlog/formatter.py
+++ jsonlog/formatter.py
@@ -4,6 +4,7 @@ import logging
 import os
 from datetime import datetime
 from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
+from uuid import UUID
 
 RESERVED_LOG_ATTRS = (
     "name",
@@ -91,9 +92,7 @@ class JSONFormatter(logging.Formatter):
         xray_trace_id = os.getenv("_X_AMZN_TRACE_ID")
         return xray_trace_id.split(";")[0].replace("Root=", "") if xray_trace_id else None
 
-    def _extract_log_exception(
-        self, log_record: logging.LogRecord
-    ) -> Union[Tuple[str, str], Tuple[None, None]]:
+    def _extract_log_exception(self, log_record: logging.LogRecord) -> Union[Tuple[str, str], Tuple[None, None]]:
 
         if log_record.exc_info and (exc_info := log_record.exc_info[0]):
             return self.formatException(log_record.exc_info), exc_info.__name__
@@ -107,7 +106,13 @@ class JSONFormatter(logging.Formatter):
         record_dict[
             "asctime"
         ] = f"{datetime.utcfromtimestamp(log_record.created):%Y-%m-%d %H:%M:%S}.{log_record.msecs:.3f}Z"
-        extras = {k: v for k, v in record_dict.items() if k not in RESERVED_LOG_ATTRS}
+
+        extras = {}
+        for k, v in record_dict.items():
+            if k not in RESERVED_LOG_ATTRS:
+                if isinstance(v, UUID) or isinstance(v, datetime):
+                    v = str(v)
+                extras[k] = v
 
         formatted = {}
 
